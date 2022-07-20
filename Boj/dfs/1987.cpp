afd92239fbd7e312ed2dfe1af464bbea72ff657d
skck0226 <skck0226@ajou.ac.kr>
2022-07-20 13:47:59 +0900

    add 1987.cpp

--- Boj/dfs/1987.cpp
+++ Boj/dfs/1987.cpp
@@ -6,40 +6,37 @@ char arr[21][21];
 int visited[21][21];
 int dy[4] = {0,0,1,-1};
 int dx[4] = {1,-1,0,0};
-int dfs(int y, int x, vector<char> picked){
-	
+vector<char> picked;
+int dfs(int y, int x){
+	visited[y][x] = 1;	
 	picked.push_back(arr[y][x]);
 	// for(int i=0;i<picked.size();i++){
 	// 	cout << picked[i] << endl;
 	// }
 	// cout << picked.size() << endl;
 	// cout << "----------" << endl;
-	int max=0;
+	int max=1;
 	for(int i=0;i<4;i++){
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 		if(visited[ny][nx]==1) continue;
 		if(ny>=n||nx>=m||ny<0||nx<0) continue;
-		int res,flag=0;
+		int res, flag=0;
 		for(int i=0;i<picked.size();i++){
 			if(arr[ny][nx]==picked[i]){
-				res = picked.size()-1;
 				flag = 1;
 				break;
 			}
 		}
-		if(flag==1){
-			if(max<res) max = res;
-			continue;
-		}
-		visited[ny][nx] = 1;	
-		res = dfs(ny,nx,picked);
+		if(flag==1) continue;
+
+		res = dfs(ny,nx);
+		if(max<res) max = res;
+
 		picked.pop_back();
 		visited[ny][nx] = 0;
-		// cout << res << endl;
-		if(max<res) max = res;
 	}
-	return max;
+	return max > picked.size() ? max : picked.size();
 }
 int main() {
 	scanf("%d %d",&n,&m);
@@ -49,10 +46,7 @@ int main() {
 			scanf("%c",&arr[i][j]);
 		}
 	}
-	vector<char> picked;
-	visited[0][0] = 1;
-	int res = dfs(0,0,picked);
-	if(res==0) cout <<"1";
-	else cout << res;
+	int res = dfs(0,0);
+	cout << res << endl;
 	return 0;
 }
